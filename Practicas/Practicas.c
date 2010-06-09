04a448b62bb8120fc90e4e1e8444ea7f5a755b86
Ander <zman7777@hotmail.es>
2010-06-09 18:05:24 +0200

    Escrita la explicacion con printf

--- Practicas/Practicas.c
+++ Practicas/Practicas.c
@@ -4,6 +4,12 @@ int main ()
 {
 	int uc=19;
 	char uf='J';
+	
+	printf("¡Bienvenido a \"Hundir la flota\"!\n\n");
+	printf("- Las medidas del tablero son: %d columnas x 10 filas.\n",uc+1);
+	printf("- La columna se indica con un numero (0, 1, 2, ..., %d)\n",uc);
+	printf("- La fila se indica con una letra mayuscula (A, B, C, ..., %c)\n",uf);
+
 
 	return 0;
 }
