6dbc321a9e4a1a6f8228641a6227e1c09ab44110
Ander <zman7777@hotmail.es>
2010-06-16 18:07:26 +0200

    2.1 completado

--- Practicas/Practicas.c
+++ Practicas/Practicas.c
@@ -1,63 +1,22 @@
 #include <stdio.h>
 
-void DibujaTablero();
-
-void DibujaTablero()
+int main ()
 {
-	int uc=19,printcolumna=0;
-	char uf='J',printfila='A';
-	printf("    __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __\n   |");
+	int uc=19,columna,numf;
+	char uf='J',fila;
 	
-	for(;printcolumna<10;)
-	{
-		printf("%d |",printcolumna);
-		printcolumna=++printcolumna;
-	}
-	for(;(printcolumna<uc+1)&&(printcolumna>9);)
-	{
-		printf("%d|", printcolumna);
-		printcolumna=++printcolumna;
-	}
-	printf("\n");
-	printf(" __|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n");
 	
-	for(;printfila<'K';)
-	{
-		printf("| %c|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |\n",printfila);
-		printf("|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n");
-		printfila=++printfila;
-	}
-}
-void main()
-{
-
-	int columna;
-	char fila,salir=0;
-
 	printf("¡Bienvenido a \"Hundir la flota\"!\n\n");
-	DibujaTablero();
+	printf("- Las medidas del tablero son: %d columnas x 10 filas.\n",uc+1);
+	printf("- La columna se indica con un numero (0, 1, 2, ..., %d)\n",uc);
+	printf("- La fila se indica con una letra mayuscula (A, B, C, ..., %c)\n",uf);
 
 	do
 	{
-		printf("\nIntroduce una coordenada (ej. B12) :\n");
-		scanf("%c%d",&fila, &columna);
+	printf("\nIntroduce una coordenada (ej. B12) :\n");
+	scanf("%c%d",&fila, &columna);
+	if((fila>='a')&&(fila<='z'))
+		fila=fila-32;
 
-		if(fila>'Z')
-			fila=fila-32;
-
-		if((fila=='Z')&&(columna==0))
-		{
-			printf("\nEl juego ha sido interrumpido.\n");
-			salir=1;
-		}
-	
-		else
-		{
-			if((fila<'K')&&(columna<20))
-				printf("\nFila %c, Columna %d\n",fila,columna);
-			else
-				printf("\nError en las coordenadas\n");
-		}
-	}
-	while(salir==0);
+	return 0;
 }
